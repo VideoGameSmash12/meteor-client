b8b637a5b57b331903dc8898054c863d7da36cbd
Video <videogamesm12@gmail.com>
2024-05-01 21:13:45 -0600

    Removes problematic mixin that causes compatibility issues with CFX

--- src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
@@ -7,12 +7,10 @@ package meteordevelopment.meteorclient.mixin;
 
 import com.google.gson.JsonParser;
 import meteordevelopment.meteorclient.MeteorClient;
-import meteordevelopment.meteorclient.systems.config.Config;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.misc.Version;
 import meteordevelopment.meteorclient.utils.network.Http;
 import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
-import meteordevelopment.meteorclient.utils.player.TitleScreenCredits;
 import meteordevelopment.meteorclient.utils.render.prompts.OkPrompt;
 import meteordevelopment.meteorclient.utils.render.prompts.YesNoPrompt;
 import net.minecraft.client.gui.DrawContext;
@@ -20,12 +18,10 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
@@ -67,18 +63,6 @@ public class TitleScreenMixin extends Screen {
                     }
                 });
             }
-        }
-    }
-
-    @Inject(method = "render", at = @At("TAIL"))
-    private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (Config.get().titleScreenCredits.get()) TitleScreenCredits.render(context);
-    }
-
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> info) {
-        if (Config.get().titleScreenCredits.get() && button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
-            if (TitleScreenCredits.onClicked(mouseX, mouseY)) info.setReturnValue(true);
         }
     }
 }
